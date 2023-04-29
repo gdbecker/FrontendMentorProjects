9109cdc6c4cdaa753bfa58ede0b8e0c3eab2ca45
Garrett Becker <garrettdbecker@gmail.com>
2023-04-29 16:10:05 -0400

    Updates

--- blogr-homepage/src/components/Navigation.js
+++ blogr-homepage/src/components/Navigation.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 
 function Navigation() {
+  const [selected, setSelected] = useState(false);
+
+  const onClick = (e) => {
+    setSelected(!selected);
+  }
+
+  var navToggler = selected ? 'navbar-toggler-icon navbar-x' : 'navbar-toggler-icon navbar-burger';
+
   return (
     <div id="navigation">
-      <nav className="navbar navbar-expand-lg navbar-dark">
+      <nav className="navbar navbar-fixed-top navbar-expand-lg navbar-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="/"><h1 className="logo"><Logo></Logo></h1></a>
 
@@ -19,33 +28,62 @@ function Navigation() {
             aria-controls="navbar"
             aria-expanded="false"
             aria-label="Toggle navigation"
+            onClick={e => onClick(e)}
           >
-            <span className="navbar-toggler-icon navbar-burger"></span>
+            <span className={navToggler}></span>
           </button>
 
           <div className="collapse navbar-collapse nav-wrapper" id="navbar">
             <ul className="navbar-nav ms-left">
               <li>
-                <Dropdown>
-                  <Dropdown.Toggle variant="" id="dropdown-basic">
-                    Product
-                    
-                  </Dropdown.Toggle>
-
-                  <Dropdown.Menu>
-                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-                    <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
+                  <DropdownButton
+                    key="down-centered"
+                    drop="down-centered"
+                    title="Product"
+                    id="dropdown"
+                  >
+                    <Dropdown.Item href="/">Overview</Dropdown.Item>
+                    <Dropdown.Item href="/">Pricing</Dropdown.Item>
+                    <Dropdown.Item href="/">Marketplace</Dropdown.Item>
+                    <Dropdown.Item href="/">Features</Dropdown.Item>
+                    <Dropdown.Item href="/">Integrations</Dropdown.Item>
+                  </DropdownButton>
+              </li>
+              <li>
+                  <DropdownButton
+                    key="down-centered"
+                    drop="down-centered"
+                    title="Company"
+                    id="dropdown"
+                  >
+                    <Dropdown.Item href="/">About</Dropdown.Item>
+                    <Dropdown.Item href="/">Team</Dropdown.Item>
+                    <Dropdown.Item href="/">Blog</Dropdown.Item>
+                    <Dropdown.Item href="/">Careers</Dropdown.Item>
+                  </DropdownButton>
+              </li>
+              <li>
+                  <DropdownButton
+                    key="down-centered"
+                    drop="down-centered"
+                    title="Connect"
+                    id="dropdown"
+                  >
+                    <Dropdown.Item href="/">Contact</Dropdown.Item>
+                    <Dropdown.Item href="/">Newsletter</Dropdown.Item>
+                    <Dropdown.Item href="/">LinkedIn</Dropdown.Item>
+                  </DropdownButton>
               </li>
             </ul>
+
+            <hr className="navDivider"></hr>
+
             <ul className="navbar-nav ms-auto">
-              <li>
-                <button className="learn-button" type="button">Login</button>
+              <li className="nav-item">
+                <button className="login-button" type="button">Login</button>
               </li>
-              <li>
-                <button className="start-button" type="button">Sign Up</button>
+              <li className="nav-item">
+                <button className="signup-button" type="button">Sign Up</button>
               </li>
             </ul>
             
--- blogr-homepage/src/pages/LandingPage.js
+++ blogr-homepage/src/pages/LandingPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { ReactComponent as PicSection2 } from '../assets/illustration-editor-desktop.svg';
+import { ReactComponent as PicSection3 } from '../assets/illustration-phones.svg';
 
 import Navigation from '../components/Navigation';
 
@@ -20,8 +22,49 @@ function LandingPage() {
           <button className="start-button" type="button">Start for Free</button>
           <button className="learn-button" type="button">Learn More</button>
         </div>
-        
+      </div>
 
+      <div className="row section-2">
+        <h1 className="section-2-header">Designed for the future</h1>
+        <div className="row mt-3 section-2-content">
+          
+          <div className="col-lg-6 my-auto">
+            <div className="row mt-2">
+              <h2 className="section-2-subheader">Introducing an extensible editor</h2>
+            </div>
+            <div className="row mt-2">
+              <p className="section-2-text">
+                Blogr features an exceedingly intuitive interface which lets you focus on one thing: creating content. 
+                The editor supports management of multiple blogs and allows easy manipulation of embeds such as images, 
+                videos, and Markdown. Extensibility with plugins and themes provide easy ways to add functionality or 
+                change the looks of a blog.
+              </p>
+            </div>
+
+            <div className="row mt-2">
+              <h2 className="section-2-subheader">Robust content management</h2>
+            </div>
+            <div className="row mt-2">
+              <p className="section-2-text">
+                Flexible content management enables users to easily move through posts. Increase the usability of your blog 
+                by adding customized categories, sections, format, or flow. With this functionality, you’re in full control.
+              </p>
+            </div>
+          </div>
+
+          <div className="col-lg-6 section-2-pic-wrapper">
+            <PicSection2 className="section-2-pic"></PicSection2>
+          </div>
+          
+          
+        </div>
+      </div>
+
+      <div className="row section-3">
+        
+          
+        
+        
       </div>
       
 
