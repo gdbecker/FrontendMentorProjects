16d80b79f2d0fd0512dd405f51c445abfcd2ca8d
Garrett Becker <garrettdbecker@gmail.com>
2023-05-01 15:10:44 -0400

    Updates

--- nft-card/src/components/NFTCard.js
+++ nft-card/src/components/NFTCard.js
@@ -4,18 +4,18 @@ function NFTCard() {
   return (
     <div id="nft">
       <div className="container">
-        <div className="row">
+        <div className="row g-1">
           <div className="nft-box-wrapper">
             <a href="/" className="nft-box-link"><div className="nft-box"></div></a>
           </div>
         </div>
-        <div className="row mt-3">
+        <div className="row g-1 mt-3">
           <a href="/"><h1 className="nft-header">Equilibrium #3429</h1></a>
         </div>
-        <div className="row">
+        <div className="row g-1">
           <p className="nft-text">Our Equilibrium collection promotes balance and calm.</p>
         </div>
-        <div className="row">
+        <div className="row g-1">
           <div className="col nft-price-col">
             <p className='nft-price-text'><img src='./assets/images/icon-ethereum.svg' className="nft-price-icon"></img>0.041 ETH</p>
           </div>
@@ -23,7 +23,7 @@ function NFTCard() {
             <p className='time-text'><img src='./assets/images/icon-clock.svg' className="time-icon"></img>3 days left</p>
           </div>
         </div>
-        <div className="row">
+        <div className="row g-1">
           <div className="col">
             <hr className="divider"></hr>
           </div>          
