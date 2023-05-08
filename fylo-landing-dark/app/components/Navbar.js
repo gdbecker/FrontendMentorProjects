479324b28ff8cf31eef064bdde21e0474a9b8f95
Garrett Becker <garrettdbecker@gmail.com>
2023-05-08 12:19:51 -0400

    Updates

--- fylo-landing-dark/app/components/Navbar.js
+++ fylo-landing-dark/app/components/Navbar.js
@@ -5,7 +5,7 @@ import Logo from '../../public/logo.svg'
 function Navbar() {
   return (
     <div id="navigation">
-      <nav className="navbar navbar-expand navbar-light navbar-fixed-top mobile-nofixed">
+      <nav className="navbar navbar-expand navbar-light navbar-fixed-top mobile-fixed">
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             <h4>
@@ -18,7 +18,7 @@ function Navbar() {
             </h4>
           </a>
         
-          <ul className="nav navbar-nav ">
+          <ul className="nav navbar-nav ms-auto">
             <li className="nav-item">
               <a className="nav-link" href="/">Features</a>
             </li>
--- fylo-landing-dark/app/page.js
+++ fylo-landing-dark/app/page.js
@@ -20,11 +20,39 @@ function Home() {
   return (
     <div id="home-page">
       <div className="container">
-        <div className="row section-intro">
-
+        <div className="row">
+          <div className="section-intro-pic"></div>
         </div>
-      </div>
-    </div>
+        <div className="row mt-3">
+          <div className="col-2"></div>
+          <div className="col-8">
+            <div className="row mt-3">
+              <h1 className="section-intro-header">
+                All your files in one secure location, accessible anywhere.
+              </h1>
+            </div>
+            <div className="row mt-3">
+              <div className="col-2"></div>
+              <div className="col-8">
+                <p className="section-intro-text">
+                  Fylo stores all your most important files in one secure location. Access them wherever 
+                  you need, share and collaborate with friends family, and co-workers.
+                </p>
+              </div>
+              <div className="col-2"></div>
+            </div>
+            <div className="row mt-3">
+              <div className="col">
+                <button type="button" className="section-intro-button">Get Started</button>
+              </div>
+              
+            </div>
+          </div>
+          <div className="col-2"></div>
+         
+        </div>
+        </div>
+     </div>
   )
 }
 
