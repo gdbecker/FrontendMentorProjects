30aec08be56112faa0886373633eedd3a2373084
Garrett Becker <garrettdbecker@gmail.com>
2023-07-16 20:16:57 -0400

    Updates

---
+++ ip-tracker/app/loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LoadingPage() {
+  return (
+    <main id="loading-page">
+      <div className="text-center min-h-screen bg-darkGray font-rubikMedium">
+        <h1 className="text-offWhite text-lg py-32">Loading</h1>
+      </div>
+    </main>
+  )
+}
+
+export default LoadingPage;
--- ip-tracker/app/page.js
+++ ip-tracker/app/page.js
@@ -1,30 +1,77 @@
 'use client';
 import React, { useState, useEffect } from 'react';
+import LoadingPage from './loading';
 import Image from 'next/image';
 import IconLocation from '../public/icon-location.svg';
 import { BiChevronRight } from 'react-icons/bi';
+
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 function Home() {
 
   // State variables
   const [isLoading, setIsLoading] = useState(true);
   const [ipSearch, setIPSearch] = useState('');
+  const [data, setData] = useState({
+    idpAddress: '',
+    location: '',
+    lat: '',
+    lng: '',
+    timezone: '',
+    isp: ''
+  });
 
   // Update IP Address to search
   const onChangeIPAddress = (e) => {
     setIPSearch(e.currentTarget.value);
   }
 
-  // Set up app for viewing
+  const fetchNewData = async () => {
+
+    const API_KEY = "at_WGi1atAenbIjfq6QgqTt2DjpPrEgP"
+    var response = "";
+
+    if (ipSearch != '') {
+      // response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${ipSearch}`);
+    } else {
+      const clientResponse = await fetch(
+        'https://api.ipify.org?format=json'
+      )
+      const clientData = await clientResponse.json();
+      // response = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=${API_KEY}&ipAddress=${clientData.ip}`);
+    }
+
+    // const data = await response.json();
+    // console.log(data);
+
+    // setData({
+    //   ipAddress: data.ip,
+    //   location: data.location.city + ", " + data.location.region + " " + data.location.postalCode,
+    //   lat: data.location.lat,
+    //   lng: data.location.lng,
+    //   timezone: "UTC" + data.location.timezone,
+    //   isp: data.isp
+    // });
+
+    setIsLoading(false)
+  }
+
+  // Prep app for viewing
   useEffect(() => {
-    setIsLoading(false);
-  }, [])
+    fetchNewData();
+    // setIsLoading(false);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
 
   if (!isLoading) {
     return (
       <main className="min-h-screen bg-offWhite">
         <div className="flex h-80 md:h-64 items-center justify-center bg-cover bg-center bg-no-repeat bg-[url('../../public/pattern-bg-mobile.png')] md:bg-[url('../../public/pattern-bg-desktop.png')]">
-          <div className="relative w-full md:w-[85%] lg:w-[75%] xl:w-[75%] 2xl:w-[60%]">
+          <div className="relative w-full md:w-[85%] lg:w-[80%] xl:w-[75%] 2xl:w-[60%]">
             <div className="absolute flex-col -top-32 px-8 w-full text-center items-center justify-center md:-top-24">
               <div className="flex flex-row w-full items-center justify-center">
                 <h1 className="text-offWhite text-3xl font-rubikMedium lg:text-3xl">IP Address Tracker</h1>
@@ -40,7 +87,8 @@ function Home() {
                   required
                 />
                 <button
-                  className="p-3 bg-veryDarkGray text-offWhite text-3xl rounded-tr-xl rounded-br-xl hover:bg-darkGray">
+                  className="p-3 bg-veryDarkGray text-offWhite text-3xl rounded-tr-xl rounded-br-xl hover:bg-darkGray"
+                  onClick={() => fetchNewData()}>
                   <BiChevronRight />
                 </button>
               </div>
@@ -48,19 +96,19 @@ function Home() {
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-4 md:gap-7">
                   <div className="flex flex-col px-7 text-center md:text-left">
                     <h1 className="text-[0.6rem] text-darkGray font-rubikBold tracking-[0.08rem]">IP ADDRESS</h1>
-                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">192.212.174.072</h2>
+                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">{data.ipAddress}</h2>
                   </div>
                   <div className="flex flex-col px-7 text-center md:text-left md:border-l-[1px] md:border-darkGray">
                     <h1 className="text-[0.6rem] text-darkGray font-rubikBold tracking-[0.08rem]">LOCATION</h1>
-                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">Brooklyn, NY 10001</h2>
+                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">{data.location}</h2>
                   </div>
                   <div className="flex flex-col px-7 text-center md:text-left md:border-l-[1px] md:border-darkGray">
                     <h1 className="text-[0.6rem] text-darkGray font-rubikBold tracking-[0.08rem]">TIMEZONE</h1>
-                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">UTC-05:00</h2>
+                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">{data.timezone}</h2>
                   </div>
                   <div className="flex flex-col px-7 text-center md:text-left md:border-l-[1px] md:border-darkGray">
                     <h1 className="text-[0.6rem] text-darkGray font-rubikBold tracking-[0.08rem]">ISP</h1>
-                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">SpaceX Starlink</h2>
+                    <h2 className="mt-2 text-xl text-veryDarkGray font-rubikMedium">{data.isp}</h2>
                   </div>
                   
                 </div>
@@ -70,7 +118,18 @@ function Home() {
 
             </div>
           </div>     
-        </div>      
+        </div>
+        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} className="flex w-full min-h-screen">
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[51.505, -0.09]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>   
       </main>
     )
   }
