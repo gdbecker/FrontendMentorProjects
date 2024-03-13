0dc56f6604b3f62ae5e21625df2cf5f6ff1b483c
gdbecker <53883172+gdbecker@users.noreply.github.com>
2024-03-13 13:50:02 -0400

    Updates

--- rock-paper-scissors/script.js
+++ rock-paper-scissors/script.js
@@ -1,8 +1,36 @@
+// Show rules images
 const rulesBtn = document.getElementById('rules-btn');
 const rulesCloseBtn = document.getElementById('rules-close-btn');
 const rulesModal = document.getElementById('rules-modal');
 const rulesModalBg = document.getElementById('rules-modal-bg');
 
+// Controlling game mode
+const toggle = document.getElementById('toggle');
+const mainRulesImg = document.getElementById('rules-img');
+const bonusRulesImg = document.getElementById('bonus-rules-img');
+const mainLogo = document.getElementById('main-logo');
+const bonusLogo = document.getElementById('bonus-logo');
+const mainGame = document.getElementById('main');
+const bonusGame = document.getElementById('bonus');
+let mode = 'Main';
+
+// Result section
+const resultSection = document.getElementById('result');
+const placeholder = document.getElementById('placeholder');
+const userCol = document.getElementById('you');
+const compCol = document.getElementById('house');
+const playAgain = document.getElementById('play-again');
+
+// Key variables
+const mainOptions = ['rock', 'paper', 'scissors'];
+const bonusOptions = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
+let mainScore = +JSON.parse(localStorage.getItem('main'));
+let bonusScore = +JSON.parse(localStorage.getItem('bonus'));
+const score = document.getElementById('score');
+const gameButtons = document.querySelectorAll('.btn');
+
+
+// Controlling game mode and showing/closing rules
 rulesBtn.addEventListener('click', () => {
 	// Show the rules modal
 	rulesModal.classList.remove('hidden');
@@ -14,3 +42,157 @@ rulesCloseBtn.addEventListener('click', () => {
 	rulesModal.classList.add('hidden');
 	rulesModalBg.classList.add('hidden');
 });
+
+toggle.addEventListener('change', (e) => {
+	// Change game mode
+	if (toggle.checked) {
+		mainRulesImg.classList.add('hidden');
+		bonusRulesImg.classList.remove('hidden');
+		mainLogo.classList.add('hidden');
+		bonusLogo.classList.remove('hidden');
+		mainGame.classList.add('hidden');
+		bonusGame.classList.remove('hidden');
+		mode = 'Bonus';
+	} else {
+		mainRulesImg.classList.remove('hidden');
+		bonusRulesImg.classList.add('hidden');
+		mainLogo.classList.remove('hidden');
+		bonusLogo.classList.add('hidden');
+		mainGame.classList.remove('hidden');
+		bonusGame.classList.add('hidden');
+		mode = 'Main';
+	}
+});
+
+// Trigger game when user clicks a game button
+gameButtons.forEach(btn => {
+	btn.addEventListener('click', (btn) => playGame(btn));
+});
+
+// Play game
+function playGame(btn) {
+	// Get players' moves
+	const user = btn.srcElement.id;
+	let comp = '';
+	let winner = '';
+	
+	if (mode === 'Main') {
+		comp = mainOptions[Math.floor(Math.random() * mainOptions.length)];
+
+		// Decide winner
+		winner = getWinnerMain(user, comp);
+
+		// Hide main game
+		mainGame.classList.add('hidden');
+	} else {
+		comp = bonusOptions[Math.floor(Math.random() * bonusOptions.length)];
+
+		// Decide winner
+		winner = getWinnerBonus(user, comp);
+
+		// Hide main game
+		bonusGame.classList.add('hidden');
+	}
+
+	// Show results page
+	resultSection.classList.remove('hidden');
+
+	setTimeout(() => {
+		buildResultBtn(user, userCol);
+
+		setTimeout(() => {
+			placeholder.classList.add('hidden');
+
+			setTimeout(() => {
+				buildResultBtn(comp, compCol);
+
+				setTimeout(() => {
+					if (winner === 'user') {
+						const button = userCol.querySelector('.btn');
+						button.classList.add('winner');
+					} else if (winner === 'comp') {
+						const button = compCol.querySelector('.btn');
+						button.classList.add('winner');
+					}
+
+					playAgain.classList.remove('hidden');
+					const h1 = document.createElement('h1');
+					h1.classList.add('play-again-title');
+					
+					if (winner === 'user') {
+						h1.innerText = 'YOU WON!';
+
+						if (mode === 'Main') {
+							mainScore++;
+							localStorage.setItem('main', JSON.stringify(mainScore));
+						} else {
+							bonusScore++;
+							localStorage.setItem('bonus', JSON.stringify(bonusScore));
+						}
+						
+					} else if (winner === 'comp') {
+						h1.innerText = 'YOU LOSE';
+					} else {
+						h1.innerText = 'DRAW!';
+					}
+
+					playAgain.insertBefore(h1, playAgain.firstChild);
+					getScores();
+ 					
+				}, 1000);
+			}, 100);
+		}, 1500);
+	}, 100);
+
+}
+
+// Get game winner (main mode)
+function getWinnerMain(user, comp) {
+	if (user === comp) {
+		return 'draw';
+	}
+
+	if ((user === 'rock' && comp === 'paper') || (user === 'paper' && comp === 'scissors') || (user === 'scissors' && comp === 'rock')) {
+		return 'comp';
+	} else {
+		return 'user';
+	}
+}
+
+// Get game winner (bonus mode)
+function getWinnerBonus(user, comp) {
+
+}
+
+function buildResultBtn(player, col) {
+	const div = document.createElement('div');
+	div.classList.add('btn');
+	div.classList.add('btn-result');
+	div.id = player;
+	div.innerHTML = `
+		<div class="btn-img">
+		<img src="./public/icon-${player}.svg" alt="${player}">
+	</div>
+	`;
+	col.appendChild(div);
+}
+
+// Get localStorage game scores
+function getScores() {
+	// if (mainScore === null) {
+	// 	mainScore = 0;
+	// }
+
+	// if (bonusScore === null) {
+	// 	bonusScore = 0;
+	// }
+
+	if (mode === 'Main') {
+		score.innerText = mainScore;
+	} else {
+		score.innerText = bonusScore;
+	}
+}
+
+// Initialize
+getScores();
